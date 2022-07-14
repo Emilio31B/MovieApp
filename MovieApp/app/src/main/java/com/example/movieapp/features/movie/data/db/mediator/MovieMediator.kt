3a5b6cac9a893d92b8a06f328db981c977c0efca
Emilio Borjas <borjas.emilio31@gmail.com>
2022-07-13 21:27:19 -0500

    delete some comments

--- MovieApp/app/src/main/java/com/example/movieapp/features/movie/data/db/mediator/MovieMediator.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/features/movie/data/db/mediator/MovieMediator.kt
@@ -30,22 +30,12 @@ class MovieMediator(
             }
             LoadType.PREPEND -> {
                 val remoteKeys = getRemoteKeyForFirstItem(state)
-                // If remoteKeys is null, that means the refresh result is not in the database yet.
-                // We can return Success with `endOfPaginationReached = false` because Paging
-                // will call this method again if RemoteKeys becomes non-null.
-                // If remoteKeys is NOT NULL but its prevKey is null, that means we've reached
-                // the end of pagination for prepend.
                 val prevKey = remoteKeys?.prevKey
                     ?: return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
                 prevKey
             }
             LoadType.APPEND -> {
                 val remoteKeys = getRemoteKeyForLastItem(state)
-                // If remoteKeys is null, that means the refresh result is not in the database yet.
-                // We can return Success with endOfPaginationReached = false because Paging
-                // will call this method again if RemoteKeys becomes non-null.
-                // If remoteKeys is NOT NULL but its prevKey is null, that means we've reached
-                // the end of pagination for append.
                 val nextKey = remoteKeys?.nextKey
                     ?: return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
                 nextKey
@@ -58,7 +48,6 @@ class MovieMediator(
             val movies = apiResponse.body()!!.results.filter { true }
             val endOfPaginationReached = movies?.isEmpty()
             movieDatabase.withTransaction {
-                // clear all tables in the database
                 if (loadType == LoadType.REFRESH) {
                     movieDatabase.remoteKeysDao().clearRemoteKeys()
                     movieDatabase.movieDao().clearMovies()
@@ -82,21 +71,15 @@ class MovieMediator(
     }
 
     private suspend fun getRemoteKeyForLastItem(state: PagingState<Int, Movie>): RemoteKey? {
-        // Get the last page that was retrieved, that contained items.
-        // From that last page, get the last item
         return state.pages.lastOrNull() { it.data.isNotEmpty() }?.data?.lastOrNull()
             ?.let { movie ->
-                // Get the remote keys of the last item retrieved
                 movieDatabase.remoteKeysDao().remoteKeysRepoId(movie.id)
             }
     }
 
     private suspend fun getRemoteKeyForFirstItem(state: PagingState<Int, Movie>): RemoteKey? {
-        // Get the first page that was retrieved, that contained items.
-        // From that first page, get the first item
         return state.pages.firstOrNull { it.data.isNotEmpty() }?.data?.firstOrNull()
             ?.let { movie ->
-                // Get the remote keys of the first items retrieved
                 movieDatabase.remoteKeysDao().remoteKeysRepoId(movie.id)
             }
     }
@@ -104,8 +87,6 @@ class MovieMediator(
     private suspend fun getRemoteKeyClosestToCurrentPosition(
         state: PagingState<Int, Movie>
     ): RemoteKey? {
-        // The paging library is trying to load data after the anchor position
-        // Get the item closest to the anchor position
         return state.anchorPosition?.let { position ->
             state.closestItemToPosition(position)?.id?.let { repoId ->
                 movieDatabase.remoteKeysDao().remoteKeysRepoId(repoId)
